f877b4e3045d046c2c73866043a25a22a65bebd9
Sanna Mäkinen <sanna.makinen@gmail.com>
2024-01-06 09:59:51 +0200

    Add reverse proxy

--- src/web/sites/default/settings.php
+++ src/web/sites/default/settings.php
@@ -29,6 +29,11 @@ $databases['default']['default'] = array (
     'driver' => 'mysql',
 );
 
+if (getenv('SITE_URL') !== null && !empty(getenv('SITE_URL'))) {
+  $settings['reverse_proxy'] = TRUE;
+  $settings['reverse_proxy_addresses'] = ['0.0.0.0'];
+}
+
 // Automatically generated include for settings managed by ddev.
 $ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
 if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
